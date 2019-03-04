6dc17d265923904dd31e2c17db0455f2ebdee177
Jeremy Airey <jairey@digitalfunkmachine.com>
2019-03-04 15:17:21 -0800

    more logic.  started working on tiles field generation.  Need to work on tile editor soon.

--- Assets/Scripts/BootSeq.cs
+++ Assets/Scripts/BootSeq.cs
@@ -8,6 +8,16 @@ public class BootSeq : MonoBehaviour
     public GameObject[] BootSequence;
     public int DelayTime;
 
+    void Awake()
+    {
+
+        BootSequence[0].SetActive(true);   // Show title screen
+        BootSequence[1].SetActive(false); // Hide Create a Character
+        BootSequence[2].SetActive(false); // Hide gameplay screens
+
+
+    }
+
     void Start()
     {
 
@@ -17,16 +27,12 @@ public class BootSeq : MonoBehaviour
         //1 = Create Character
         //2 = GamePlay
 
-            BootSequence[0].SetActive(true);   // Show title screen
-            BootSequence[1].SetActive(false); // Hide Create a Character
-            BootSequence[2].SetActive(false); // Hide gameplay screens
-        Debug.Log("starting sleep");
-
+            //BootSequence[0].SetActive(true);   // Show title screen
+            //BootSequence[1].SetActive(false); // Hide Create a Character
+            //BootSequence[2].SetActive(false); // Hide gameplay screens
+        
         Invoke("ShowCharacterSelect", DelayTime);
-
-      
-
-    }
+            }
 
  
     IEnumerator WaitForIt(float waitTime)
@@ -38,13 +44,22 @@ public class BootSeq : MonoBehaviour
     
     void ShowCharacterSelect()
     {
-        Debug.Log("finished sleeping");
-
         BootSequence[1].SetActive(true);  //show character screen
         BootSequence[0].SetActive(false); // hide title screen
+
+        Invoke("ShowGameField", DelayTime);
+
     }
 
 
+    void ShowGameField()
+    {
+        BootSequence[2].SetActive(true); // Show gameplay screen
+        BootSequence[1].SetActive(false);  //Hide character select screen
+               
+
+
+     }
 
 
 
---
+++ Assets/Scripts/GeneratePlayField.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GeneratePlayField : MonoBehaviour
+{
+    public GameObject[] Tiles;
+    public enum TileTypes { Blank, Cave, Crypt, DeadEnd,Gulag, Room, Shrine, Tower};
+    public enum TileLocked { Locked, Open};
+    public enum Shit { };
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
